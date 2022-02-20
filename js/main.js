691b3481fa76c4a3fd070dfaffae4a0611566f97
Danichok <99190797+Danichok@users.noreply.github.com>
2022-02-21 01:41:52 +0200

    contacts

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ var currentSlide = 0;
 var screen = $('body').width()
 window.addEventListener("mousewheel", function(e) {
     wDelta = e.wheelDelta < 0 ? 'down' : 'up';
-    if (screen > 1024) {
+    if (screen > 1240) {
         if (isActive == true) {
             if (wDelta == 'down') {
                 scrollDown()
@@ -103,57 +103,7 @@ function scrollUp() {
 
 }
 
-$('.slider_button_left').click(function() {
-    var currentRotate = $('.slider_button_left').attr('style')
-    console.log(currentRotate)
-    if (currentRotate == 'transform: rotate(1turn);') {
-        $('.slider_button_left').attr('style', 'transform: rotate(0turn);')
-    } else {
-        $('.slider_button_left').attr('style', 'transform: rotate(1turn);')
-    }
-    sliderLeft()
-})
-$('.slider_button_right').click(function() {
-    var currentRotate = $('.slider_button_right').attr('style')
-    console.log(currentRotate)
-    if (currentRotate == 'transform: rotate(1turn);') {
-        $('.slider_button_right').attr('style', 'transform: rotate(0turn);')
-    } else {
-        $('.slider_button_right').attr('style', 'transform: rotate(1turn);')
-    }
-    sliderRight()
-})
-var sliderNumber = $('.slider_wrapper_photo').length // Визначення кількості слайдів
-var sliderWidth = $('.slider_wrapper_photo').width() // Ширина  слайда
-var sliderCount = 1
 
-function sliderRight() {
-    sliderCount++ // +1 до щьотчика слайда
-    console.log(sliderCount)
-    if (sliderCount > sliderNumber) {
-        sliderCount = sliderNumber
-        return false
-    } else {
-        var translateWidth = sliderWidth * (sliderCount - 1) // Ширина прокрута
-        $('.slider_wrapper').attr('style', 'transform:translateX(-' + translateWidth + 'px)')
-    }
-}
-
-function sliderLeft() {
-    sliderCount-- // +1 до щьотчика слайда
-    console.log(sliderCount)
-    if (sliderCount < 1) {
-        sliderCount = 1
-        var translateWidth = 0 // Ширина прокрута
-        $('.slider_wrapper').attr('style', 'transform:translateX(-' + translateWidth + 'px)')
-        return false
-    } else {
-        //alert('SCAm')
-        var translateWidth = sliderWidth * (sliderCount - 1) // Ширина прокрута
-        $('.slider_wrapper').attr('style', 'transform:translateX(-' + translateWidth + 'px)')
-    }
-
-}
 $('#home_menu').click(function() {
     $('.main_block_wrapper').attr('style', 'transform:translateY(-' + 0 + 'vh)')
     $('.scroll_menu_point').attr('style', 'transform: translateY(0%);');
